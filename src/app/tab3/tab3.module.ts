9fbf22f40db33791594012233f94e9570a47b1f3
N Yesid Garcia <yesid19999@gmail.com>
2022-07-25 14:48:04 -0500

    charts

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,3 +1,4 @@
+import { NgChartsModule } from 'ng2-charts';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
@@ -16,6 +17,7 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
+    NgChartsModule
   ],
   declarations: [Tab3Page]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartOptions, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { Animal } from '../interfaces/animal.interface';
+import { AnimalService } from '../services/animal.service';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  
+  myAnimals: Animal[] = [];
 
-  constructor() {}
+  // Pie
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+  };
+  public pieChartLabels = [ 'Sanos', 'Enfermos'];
+  public pieChartDatasets = [ {
+    data: [ 1, 1 ]
+  } ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
+  constructor(private animalServiceanimal: AnimalService) {
+ 
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.myAnimals = JSON.parse(JSON.stringify(this.animalServiceanimal.myAnimals)); 
+
+    setTimeout(() => {
+
+      let sanos = this.myAnimals.filter(e => e.isHealth === true).length;
+      let enfermos = this.myAnimals.filter(e => e.isHealth === false).length;
+    
+      this.pieChartDatasets = [ {
+        data: [ sanos, enfermos ]
+      } ]
+    }, 2000)
+  }
+
+  
 }
